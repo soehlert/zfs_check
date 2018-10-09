9bdac9c80f873543e014bdebdbc26c8c2ea7510b
Sam Oehlert <sam.oehlert@gmail.com>
2018-10-09 01:29:19 -0500

    zfscheck works now.

--- zfscheck.py
+++ zfscheck.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-import datetime
 import os
+import datetime
 
 from configparser import ConfigParser
 from slackclient import SlackClient
@@ -11,7 +11,7 @@ conf.read(["/etc/zfscheck.conf"])
 slack_channel = conf.get("slack", "channel_id")
 slack_token = conf.get("slack", "token")
 max_capacity = conf.get("zfs", "max_capacity")
-max_scrub_age = conf.get("zfs", "max_scrub_age")
+max_scrub_age = int(conf.get("zfs", "max_scrub_age"))
 
 slack_client = SlackClient(slack_token)
 
@@ -38,41 +38,82 @@ def get_pools():
     return pools
 
 
-def pool_status(pools):
-    """ Pool info using the status """
-    all_status = os.popen("zpool status -x").readlines()
-    if "all pools are healthy" not in all_status:
-        status = True
+def get_pool_status(name, pool):
+    """ Get ZFS status info """
+    status_cmd = "zpool status -x {}".format(name)
+    status = os.popen(status_cmd).read()
 
-    return status
-
-
-def pool_scrub(pools):
-    """ Check for last scrub time """
-
-    for pool in pools:
-        scrub_cmd = "zpool status {} | grep scrub".format(pool)
-        scrub = os.system(scrub_cmd)
-        print(type(scrub))
-
-    return
+    return status.strip()
 
 
-def pool_health(pools):
-    """ Check for any health issues """
-    for pool in pools:
-        pool_name = pool.split()[0]
-        pool_capacity = pool.split()[6]
-        pool_health = pool.split()[8]
+def get_pool_health(pool):
+    """ Get ZFS list info """
+    pool_name = pool.split()[0]
+    pool_capacity = pool.split()[6]
+    pool_health = pool.split()[8]
 
     return pool_name, pool_capacity, pool_health
 
 
+def get_scrub_date(pool):
+    """ Check for last scrub time """
+    pool_name = pool.split()[0]
+    scrub_cmd = "zpool status {} | grep scrub | awk '{{ print $15 $12 $13}}'".format(
+        pool_name
+    )
+    scrub_date = os.popen(scrub_cmd).readlines()
+
+    return scrub_date
+
+
+def pool_warning(name, status):
+    """ Check if we need to send a warning about pool status """
+    status_check = "pool '{}' is healthy".format(name)
+    if status != status_check:
+        err_cmd = "zpool status {}".format(name)
+        pool_status = os.popen(err_cmd).read()
+        msg = "WARNING: {} status is not healthy\n\n{}".format(name, pool_status)
+        send_message(slack_channel, msg)
+
+    return
+
+
+def health_warning(name, capacity, health):
+    """ Check if we need to send a warning about pool health """
+    if capacity >= max_capacity:
+        msg = "WARNING: Capacity over max limit {} on {}".format(capacity, name)
+        send_message(slack_channel, msg)
+
+    if health != "ONLINE":
+        msg = "WARNING: {} is status - {}".format(name, health)
+        send_message(slack_channel, msg)
+
+    return
+
+
+def scrub_warning(name, pool):
+    """ Check if scrub is within our window """
+    today = datetime.date.today()
+
+    if pool:
+        scrub_date = datetime.datetime.strptime(pool[0].strip(), "%Y%b%d").date()
+        cutoff = today - datetime.timedelta(days=max_scrub_age)
+        if scrub_date < cutoff:
+            msg = "WARNING: Last scrub on {} was {}, please run a scan".format(
+                name, scrub_date
+            )
+            send_message(slack_channel, msg)
+    else:
+        return
+
+
 def main():
-    today = datetime.datetime.today().strftime("%Y%b%d")
     pools = get_pools()
-    name, capacity, health = pool_health(pools)
-    pool_scrub(pools)
+    for p in pools:
+        name, capacity, health = get_pool_health(p)
+        health_warning(name, capacity, health)
+        scrubs = get_scrub_date(p)
+        scrub_warning(name, scrubs)
 
 
 if __name__ == "__main__":
