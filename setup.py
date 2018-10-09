0f7c105435bf3a75063985128c327b53b91e2b23
Sam Oehlert <sam.oehlert@gmail.com>
2018-10-09 05:52:39 -0500

    add license file, add setup.py file for packaging and clean up naming to match packaging.

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="zfs_check",
+    version="1.2.0",
+    description="Check on the health of your ZFS pools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # This should be a valid link to your project's main homepage.
+    #
+    # This field corresponds to the "Home-Page" metadata field:
+    # https://packaging.python.org/specifications/core-metadata/#home-page-optional
+    # url='https://github.com/pypa/sampleproject',  # Optional
+    author="Sam Oehlert",
+    author_email="sam.oehlert@gmail.com",
+    classifiers=[  # Optional
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Topic :: System :: Filesystems",
+        "Topic :: System :: Monitoring",
+    ],
+    keywords="zfs health monitor",
+    py_modules=["zfs_check"],
+    entry_points={"console_scripts": ["zfs_check=sample:main"]},
+)
